72af74c06044d3471ff0859a1db54ba0e78f5470
rafall92 <latka.rafal@gmail.com>
2026-03-17 20:24:47 +0100

    init commit

---
+++ js/script.js
@@ -0,0 +1,67 @@
+const reveals = document.querySelectorAll('.reveal');
+window.addEventListener('scroll', () => {
+    reveals.forEach(el => {
+        if (el.getBoundingClientRect().top < window.innerHeight - 100) {
+            el.classList.add('active');
+        }
+    });
+});
+
+// particles
+const canvas = document.getElementById('particles');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let particles = [];
+for (let i = 0; i < 80; i++) {
+    particles.push({
+        x: Math.random() * canvas.width,
+        y: Math.random() * canvas.height,
+        r: Math.random() * 2
+    });
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    particles.forEach(p => {
+        p.y += 0.9;
+        if (p.y > canvas.height) p.y = 0;
+        ctx.beginPath();
+        ctx.arc(p.x, p.y, p.r, 10, Math.PI * 12);
+        ctx.fillStyle = '#ff2d2d';
+        ctx.fill();
+    });
+    requestAnimationFrame(draw);
+}
+
+//draw();
+
+// SCROLL SPY
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav a");
+
+window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach(sec => {
+        const top = window.scrollY;
+        const height = sec.offsetHeight;
+        const offset = sec.offsetTop - 0.6 * sec.offsetHeight;
+
+        if (top >= offset && top < offset + height) {
+            current = sec.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove("active");
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+    });
+});
+
+if (window.location.pathname.endsWith("index.html")) {
+    window.location.replace("/");
+  }
